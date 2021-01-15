78b95d2d7bca81232d8312484ee4889625ec7435
billyboy118 <bill_matley@hotmail.com>
2021-01-15 18:18:00 +0000

    Developing classes

--- board.rb
+++ board.rb
@@ -1,10 +1,32 @@
+# frozen_string_literal: false
+
 
 
 # squares to build up the board
 class Square
 
-  def initize
+  def initialize
+    @position = nil
+    @current_piece = nil
 
+    @front = nil
+    @back = nil
+    @left = nil
+    @right = nil
+  end
+
+end
+
+# This class is used to create a board using the squares from th Square class
+class Board
+
+  def initialize
+    @board = create_board
+  end
+
+  def create_board
+
+    square = Square.new()
   end
 
 end
---
+++ start.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: false
+
+require_relative 'board'
+
+# The Knight class to demonstrate the piece moving around the board
+class Knight
+  def initialize(start_pos, end_pos)
+    @start = start_pos
+    @end_pos = end_pos
+    @forward = 2
+    @direction = 1
+  end
+end
+
+puts 'This is Knights Travails please enter your start and end coordinates'
+# I can add some validation here to ensure correct coordinates are
+# added would use 2 gets commands one for the start and one for the end
+# coordinates = gets.chomp
+
+# horizontal first and then vertical with coordinates ---> ^
+
+knight = Knight.new([0, 0] [1, 2])
+
+
+
+
