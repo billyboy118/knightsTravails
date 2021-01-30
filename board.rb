0cbc9350a05057278b8c2361fa9f9ccf52f5de03
billyboy118 <bill_matley@hotmail.com>
2021-01-30 16:24:34 +0000

    Pretty much completed version, just need to add the 2 inputs from the user and tidy up the code

--- board.rb
+++ board.rb
@@ -6,7 +6,6 @@ require_relative 'modules'
 class Square
   attr_accessor :position, :front, :back, :left, :right, :current_piece, :visited, :end_square
 
-
   def initialize(place)
     @position = place
     @current_piece = nil
@@ -40,12 +39,10 @@ class Board
   end
 
   def calculate_knight(start, finish)
-    @@finish = finish
     start = find_square(start)
     find_square(finish).end_square = true
 
     start.current_piece = Knight.new
-    start.current_piece.squares_visited = start.position
     puts start.current_piece.squares_visited
     paths(start)
   end
@@ -53,143 +50,57 @@ class Board
   def paths(start)
     queue = [start]
     order = []
-
     while queue.length.positive?
       node = queue.pop
       next if node.nil? || node.position.nil?
 
+      return return_value(node) if node.end_square == true
 
-      if node.end_square == true
-        node.current_piece.squares_visited.push(node.position)
-        return p node.current_piece.squares_visited
-      end
       order.push(node.position)
-
-
-
       queue.unshift(find_nodes(node)).flatten!
+    end
+  end
 
-      
-
-
+  def return_value(node)
+    node.current_piece.squares_visited.push(node.position)
+    puts "You made it in #{node.current_piece.squares_visited.length - 1} moves! Here's your path:"
+    node.current_piece.squares_visited.each_with_index do |place, index|
+      if index.zero?
+        puts "Start: #{place}"
+      else
+        puts "  #{index}:   #{place}"
+      end
     end
   end
 
   def find_nodes(node)
     temp_arr = []
-    temp_arr.push(front_left(node))
-    temp_arr.push(front_right(node))
-    # temp_arr.push(back_left(node))
-    # temp_arr.push(back_right(node))
-    # temp_arr.push(left_front(node))
-    # temp_arr.push(left_back(node))
-    # temp_arr.push(right_front(node))
-    # temp_arr.push(right_back(node))
+    nodes = [node.front.front.left, node.front.front.right, node.back.back.left, node.back.back.right,
+             node.left.left.front, node.left.left.back, node.right.right.front, node.right.right.back]
+    nodes.each do |new_node|
+      temp_arr.push(splint(node, new_node))
+    end
     temp_arr.reverse!
+  end
+
+  def splint(node, new_node)
+    if new_node.nil? || new_node.position.nil?
+      nil
+    elsif new_node.end_square == true
+      journey(node, new_node)
+      new_node
+    else
+      journey(node, new_node)
+      new_node
+    end
   end
 
   def journey(node, new_node)
     knight = Knight.new
     new_node.current_piece = knight
-    knight.squares_visited.push(node.current_piece.squares_visited)
+    node.current_piece.squares_visited.each do |location|
+      knight.squares_visited.push(location)
+    end
     knight.squares_visited.push(node.position)
   end
-
-  def front_left(node)
-    new_node = node.front.front.left
-    if new_node.nil? || new_node.position.nil?
-      nil
-    elsif new_node.end_square == true
-      journey(node, new_node)
-      new_node
-    else
-      journey(node, new_node)
-      new_node
-    end
-  end
-
-  def front_right(node)
-    new_node = node.front.front.right
-    if new_node.nil? || new_node.position.nil?
-      nil
-    elsif new_node.end_square == true
-      journey(node, new_node)
-      new_node
-    else
-      journey(node, new_node)
-      new_node
-    end
-  end
-
-  # def back_left(node)
-  #   node = node.back.back.left
-  #   if node.nil? || node.position.nil?
-  #     nil
-  #   elsif node.end_square == true
-  #     node
-  #   else
-  #     node
-  #   end
-  #   node
-  # end
-
-  # def back_right(node)
-  #   node = node.back.back.right
-  #   if node.nil? || node.position.nil?
-  #     nil
-  #   elsif node.end_square == true
-  #     node
-  #   else
-  #     node
-  #   end
-  #   node
-  # end
-
-  # def left_front(node)
-  #   node = node.left.left.front
-  #   if node.nil? || node.position.nil?
-  #     nil
-  #   elsif node.end_square == true
-  #     node
-  #   else
-  #     node
-  #   end
-  #   node
-  # end
-
-  # def left_back(node)
-  #   node = node.left.left.back
-  #   if node.nil? || node.position.nil?
-  #     nil
-  #   elsif node.end_square == true
-  #     node
-  #   else
-  #     node
-  #   end
-  #   node
-  # end
-
-  # def right_front(node)
-  #   node = node.right.right.front
-  #   if node.nil? || node.position.nil?
-  #     nil
-  #   elsif node.end_square == true
-  #     node
-  #   else
-  #     node
-  #   end
-  #   node
-  # end
-
-  # def right_back(node)
-  #   node = node.right.right.back
-  #   if node.nil? || node.position.nil?
-  #     nil
-  #   elsif node.end_square == true
-  #     node
-  #   else
-  #     node
-  #   end
-  #   node
-  # end
 end
