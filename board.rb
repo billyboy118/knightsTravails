5b19318471a4d59032ed35c6908dbca1cd2805cf
billyboy118 <bill_matley@hotmail.com>
2021-03-14 15:19:51 +0000

    Added methods to find positions and clean data

--- board.rb
+++ board.rb
@@ -39,34 +39,53 @@ class Board
     end
   end
 
+  def calculate_knight(start, finish)
+    start = find_square(start)
+    find_square(finish).end_square = true
+
+    start.current_piece = Knight.new
+
+    puts start.current_piece.squares_visited
+    paths(start)
+  end
+
   def find_square(value)
     board.each do |square|
       return square if square.position == value
     end
   end
 
-  def calculate_knight(start, finish)
-    start = find_square(start)
-    find_square(finish).end_square = true
-
-    start.current_piece = Knight.new
-    puts start.current_piece.squares_visited
-    paths(start)
-  end
-
   def paths(start)
     queue = [start]
-    order = []
+    # order = []
     while queue.length.positive?
       node = queue.pop
-      next if node.nil? || node.position.nil?
-
+      puts node
       return return_value(node) if node.end_square == true
 
-      order.push(node.position)
-      queue.unshift(find_nodes(node)).flatten!
+      queue.unshift(find_positions(node)).flatten!
+      # order.push(node.position)
     end
-  end
+  end   
+
+
+
+
+
+  # def paths(start)
+  #   queue = [start]
+  #   order = []
+  #   while queue.length.positive?
+  #     node = queue.pop
+  #     next if node.nil? || node.position.nil?
+
+  #     return return_value(node) if node.end_square == true
+
+  #     order.push(node.position)
+  #     queue.unshift(find_nodes(node)).flatten!
+  #   end
+  # end
+
 
   def return_value(node)
     node.current_piece.squares_visited.push(node.position)
@@ -80,15 +99,36 @@ class Board
     end
   end
 
-  def find_nodes(node) # rubocop:disable Metrics/AbcSize
+  def find_positions(node)
     temp_arr = []
-    nodes = [node.front.front.left, node.front.front.right, node.back.back.left, node.back.back.right,
-             node.left.left.front, node.left.left.back, node.right.right.front, node.right.right.back]
-    nodes.each do |new_node|
-      temp_arr.push(splint(node, new_node))
+    knight_moves = [[-1, 2], [1, 2], [2, 1], [2, -1], [-1, -2], [1, -2], [-2, 1], [-2, -1]]
+    knight_moves.each do |move|
+      temp_arr.push([node.position[0] + move[0], node.position[1] + move[1]])
     end
-    temp_arr.reverse!
+    clean_data(temp_arr)
   end
+
+  def clean_data(array)
+    temp_arr = []
+    array.each do |item|
+      temp_arr.push(item) if (item[0] >= 0 && item[0] < 8) && (item[1] >= 0 && item[1] < 8)
+    end
+  end
+
+
+
+
+
+  # def find_node(node) # rubocop:disable Metrics/AbcSize
+  #   temp_arr = []
+
+  #   nodes = [node.front.front.left, node.front.front.right, node.back.back.left, node.back.back.right,
+  #            node.left.left.front, node.left.left.back, node.right.right.front, node.right.right.back]
+  #   nodes.each do |new_node|
+  #     temp_arr.push(splint(node, new_node))
+  #   end
+  #   temp_arr.reverse!
+  # end
 
   def splint(node, new_node)
     if new_node.nil? || new_node.position.nil?
